be5fab4dcd90b2eac2fe867a9faa66accb523cc3
JY <kimamugessi@gmail.com>
2026-03-11 10:55:21 +0900

    0

--- WindowsFormsApp_1203/Inspect/InspWorker.cs
+++ WindowsFormsApp_1203/Inspect/InspWorker.cs
@@ -12,7 +12,7 @@ using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Diagnostics; // Stopwatch 사용
+using System.Diagnostics; // Stopwatch 사용 
 
 namespace JYVision.Inspect
 {
